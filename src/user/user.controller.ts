442b3d0337a3fbd2a7b81dc260a6ec587cbaea0e
Ali Ataf <aliattaf98@gmail.com>
2023-05-07 19:35:37 +0400

    feat: add jwt guard and get user decorator and implement get user api

---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+
+import { JwtGuard } from '../auth/guard';
+import { GetUser } from 'src/auth/decorator';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @Get('me')
+  getMe(@GetUser('id') userId): number {
+    return userId;
+  }
+}
